e1532e96aeebd8d1a8d26d34afaafcffe5401a70
c0decel <124975714+c0decel@users.noreply.github.com>
2023-10-13 03:09:56 -0700

    5.1

---
+++ App.js
@@ -0,0 +1,36 @@
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Start from './components/Start';
+import Chat from './components/Chat';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Screen1"
+      >
+        <Stack.Screen
+          name="Start"
+          component={Start}
+        />
+        <Stack.Screen
+          name="Chat"
+          component={Chat}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+});
