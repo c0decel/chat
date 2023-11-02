003dbbe3ec1bd2fbd1bab95d510de0ebe742d3b4
c0decel <124975714+c0decel@users.noreply.github.com>
2023-11-01 23:40:22 -0700

    5.4

--- components/Chat.js
+++ components/Chat.js
@@ -1,48 +1,143 @@
 import { useEffect, useState } from 'react';
-import { GiftedChat, Bubble } from 'react-native-gifted-chat';
-import { View, KeyboardAvoidingView, Text, StyleSheet, Alert } from 'react-native';
-import { addDoc, query, onSnapshot, collection } from 'firebase/firestore';
+import { GiftedChat, Bubble, InputToolbar, Time, Day, Avatar } from 'react-native-gifted-chat';
+import { View, KeyboardAvoidingView, StyleSheet, Text, Image} from 'react-native';
+import { addDoc, query, onSnapshot, collection, orderBy } from 'firebase/firestore';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const Chat = ({ route, navigation, db }) => {
+const Chat = ({ route, navigation, db, isConnected }) => {
   const [messages, setMessages] = useState([]);
-  const { name, backgroundColor, userID } = route.params;
+  const { name, backgroundColor, bubbleColor, userID, status } = route.params;
   const [visible, setVisible] = useState(true);
+
+  let unsubMessage;
 
   useEffect(() => {
     navigation.setOptions({ title: name });
-  }, [name, navigation]);
 
-  useEffect(() => {
-    const messageQuery = query(
-      collection(db, 'messages'),
-    );
-  
-    const unsubMessage = onSnapshot(messageQuery, (Snapshot) => {
-      const fetchedMessages = Snapshot.docs.map((doc) => {
-        const data = doc.data();
-        return {
-          _id: doc.id,
-          text: data.text,
-          createdAt: data.createdAt.toDate(),
-          user: {
-            _id: data.user._id,
-            name: data.user.name
-          },
-        };
+    if (isConnected === true) {
+      if (unsubMessage) unsubMessage();
+      unsubMessage = null;
+
+      const q = query(collection(db, 'messages'), orderBy('createdAt', 'desc'));
+      unsubMessage = onSnapshot(q, (docs) => {
+        let newMessages = [];
+        docs.forEach((doc) => {
+          newMessages.push({
+            id: doc.id,
+            ...doc.data(),
+            createdAt: new Date(doc.data().createdAt.toDate()),
+          });
+        });
+        cachedMessages(newMessages);
+        setMessages(newMessages);
       });
-  
-      fetchedMessages.sort((a, b) => b.createdAt - a.createdAt);
-  
-      setMessages(fetchedMessages);
-    });
-  
-    return () => unsubMessage();
-  }, [db]);
+    } else loadFromCache();
 
-  const onSend = (newMessages) => {
-    addDoc(collection(db, 'messages'), newMessages[0])
+    return () => {
+      if (unsubMessage) {
+        unsubMessage();
+      }
+    };
+  }, [isConnected]);
+
+  //Load cached messages
+  const loadFromCache = async () => {
+    const cachedMessages = (await AsyncStorage.getItem('messages')) || '[]';
+    setMessages(JSON.parse(cachedMessages));
   };
 
+  const cachedMessages = async (messagesToCache) => {
+    try {
+      await AsyncStorage.setItem('messages', JSON.stringify(messagesToCache));
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  //Add messages to collection
+  const onSend = (newMessages) => {
+    addDoc(collection(db, 'messages'), newMessages[0]);    
+  };
+
+  //Show user details
+  const onPressAvatar = (user) => {
+    console.log('Avatar clicked:', user);
+
+    navigation.navigate('Details', { 
+      user: {
+        name: user.name,
+        avatar: user.avatar,
+        status: user.status
+      },
+      backgroundColor: backgroundColor
+    });
+  };
+
+
+  //Renders toolbar if there is a connection
+  const renderInputToolbar = (props) => {
+    if (isConnected) return <InputToolbar {...props} />;
+    else return (
+      <View>
+        <Text style={{fontSize: 16, paddingVertical: 4, textAlign: 'center'}}>
+          Go online to compose a message
+        </Text>
+      </View>
+    )
+   }
+
+  // Text bubble color settings
+  const renderBubble = (props) => {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: bubbleColor,
+            borderColor: '#000',
+            borderWidth: 2,
+          },
+          left: {
+            backgroundColor: '#FFF',
+            borderColor: '#000',
+            borderWidth: 2
+          },
+        }}
+        textStyle={{
+          right: {
+            color: 'black'
+          }
+        }}
+      />
+    );
+  };
+
+  //Timestamp color settings
+  const renderTime = (props) => {
+    return (
+    <Time
+      {...props}
+      timeTextStyle={{ 
+        left: { color: 'black' },
+        right: { color: 'black' }
+      }}
+    />
+  );
+};
+
+  //Date color settings
+  const renderDay = (props) => {
+    return (
+      <Day
+        {...props}
+        textStyle={{
+          color: 'black'
+        }}
+      />
+    )
+  }
+
+  //Puts timer on joined message
   useEffect(() => {
     const timer = setTimeout(() => {
       setVisible(false);
@@ -55,40 +150,36 @@ const Chat = ({ route, navigation, db }) => {
  return (
   <KeyboardAvoidingView style={{ flex: 1, backgroundColor: backgroundColor }}>
     <View>
-      {visible && <Text style={{textAlign: 'center', fontSize: 18, padding: 5}}>{name} joined the chat. Say something!!!</Text>}
-      {visible && <Text style={{textAlign: 'center', padding: 5}}>User ID: {userID}</Text>}
+      {visible && <Text style={{textAlign: 'center', fontSize: 18, padding: 10}}>{name} joined the chat. Say something!!!</Text>}
     </View>
-     <GiftedChat
+     <GiftedChat style={styles.messagestext}
         messages = {messages}
         renderBubble={renderBubble}
+        renderTime={renderTime}
+        renderDay={renderDay}
         onSend={newMessages => onSend(newMessages)}
+        onPressAvatar={(user) => onPressAvatar(user)}
         user ={{
           _id: userID,
-          name: name
+          name: name,
+          status: status
         }}
+        renderInputToolbar={renderInputToolbar}
       />
    </KeyboardAvoidingView>
  );
 }
 
-const renderBubble = (props) => {
-  return (
-    <Bubble
-      {...props}
-      wrapperStyle={{
-        right: {
-          backgroundColor: '#d78cff',
-          borderColor: '#000',
-          borderWidth: 2
-        },
-        left: {
-          backgroundColor: '#FFF',
-          borderColor: '#000',
-          borderWidth: 2
-        },
-      }}
-    />
-  );
-};
+const styles = StyleSheet.create({
+  messagestext: {
+    color: '#000'
+  },
+  details: {
+    backgroundColor: '#ffffff',
+    justifyContent: 'center',
+    borderColor: '#000000',
+    borderWidth: 4
+  }
+})
 
 export default Chat;
