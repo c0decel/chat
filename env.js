84acc8431366135ec6489745dd675a93d1c5b79c
c0decel <124975714+c0decel@users.noreply.github.com>
2023-11-11 15:24:39 -0800

    Add files via upload

---
+++ env.js
@@ -0,0 +1,26 @@
+export const dialogflowConfig = 
+{
+    "type": "service_account",
+    "project_id": "fabled-lyceum-404401",
+    "private_key_id": "d22224f53eea916ceaaa8bc61be950acdedf07d3",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC8AQspUlTJBcUr\nZBW1Daz1sSPIF7cF/OdBoVWMZOCcRae5vvHW2wCvNsCf9jxG5cyG1fT6ae90hJBa\nDdxZa8+AVhBHsHztzy+ZSbEa3QeJ4MtiueadhmlL/JKBx2mACAymNR2Pk3M8dZhW\nPzS1lbJhkHyRMdd4cMiViTOe0pjJJcHn5jYPtMwgS+sZcgjbyQSpNzxtWiaK+Hi+\np5mlCZVYNawb7nVp0L8XnodI3O+TMRZpTYDfetYQWSdzMt2YRrhJehOfBn9fcnzS\nmYGsPykU2jCON2+Ee/m+4cAD7mNqltKYgpI/aEsrvKidgPPUlQUYvPXYVdVzIVxN\njykIVi6LAgMBAAECggEARe8Tck8Md+ycEAx+YKeSJ6C5oTRlIiaV9Z1of72Eqczl\nW9xyBIzZv9ObqsVDXdIclymeo8nlR5AKTF1skpzehqw9CRXXGvUfjSNS7J28sKa8\nsWBar00w/RGahAkkLvqPjgIjGWXtj1dlD+waFNP3E4DWPYJvVv+JNcrgN1u6dtPf\nvoLB4I5F3E9BYDT+ShtRYlYvrLExZc4q5YIK+jEMFXl+1/2b5wG0u/+BkaVMHutt\nx48AWRXS4Ha5Ho4O6d8rZafKqO6IQjcs/TykHH99IaNKMNlDjxTXWfQe0bKT2y3S\n1YQ+AvX718YQlnHbhCDldg/e+YanZ58QXA9XTkrNeQKBgQDpDlFAf0qq+qj4Popo\nF6rlGZSTQX+UfePwQyPs4mSoFPBoGWC7EX46aJRSiIttgHv2O9tHqVRgCFz+1Py7\nQyZ1AtTyeQ0nCBfw5E0rINOhMO9MpmxK8od+6aeQblUHqERbYALKH8qJfMLPGEAm\n+n4rLgs33Vvo14HRPoOX8lKpwwKBgQDOg0oX125/sUHhVEhabyhiBqCblY8OdQYz\ny3xca2a13T3RK6pvBQw+IVi6HPHBtQr26JPKhNvuIk4KLts8v6iK7S8/HwqR79nJ\nJyZIqScZbaklGuRja6tQfVQYKI6i4/njn/G6eFGW13wZ2uqDcG21tE5L9gNe5KyU\nmQGGHcHTmQKBgQCb9bHYGJcOPnnuzmNu70+0yt6LgMxwwaJU+YlED+jONlz/PORn\nq3gufCet56RungnWqQ3AMge90DrzafLLGk/VBBUfQnTTxu3GjfE6SFh3STkKsWBM\n82pLOu+gjsUmP6um/mQ/gPDX+v6e+hqMOxtNqB1nwCGuevFr5orHlylMYwKBgQCP\n7dj5/N+6h2QPE5rJzKcsisntJbNqtAz6lFu8Xe5b2jJ3UiOa9L2vn7YjIIEuLGvT\n4gz/HNHbinUxi66pDDr/eIdxSsUnKS90jqCUC7CAfHlirvd2g1gG5RJzM/EVfiZv\nis4C1FCo/M41hunjabxM47meH3wB6waNGoaBXsP0IQKBgEdx1r8wER+CMNcnROdW\nVuucVjMUeCpqXvsJI//MCXIj0ZgXFCRwVDqdZLnG/I5blE1I8zaDkK3CF6s+Viff\nW76aLDSTeYr/406Ah8u8fNdA4CmSxQG70RMoccpgMXIFcHg6Y3UzOBuEBlEcvp3t\nSUgRsQ8TpyqDFi+Na3i5MjPL\n-----END PRIVATE KEY-----\n",
+    "client_email": "chatbot@fabled-lyceum-404401.iam.gserviceaccount.com",
+    "client_id": "113050637625572621208",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/chatbot%40fabled-lyceum-404401.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com"
+  };
+
+  export const firebaseConfig = {
+    apiKey: "AIzaSyCqPXwoItsC1Cs6FFZi_LGQKuqqEaqe0JQ",
+    authDomain: "chat-dbe08.firebaseapp.com",
+    projectId: "chat-dbe08",
+    storageBucket: "chat-dbe08.appspot.com",
+    messagingSenderId: "378836921744",
+    appId: "1:378836921744:web:d3bc4223829c7ec57e9715",
+    measurementId: "G-RWBTB4CG08"
+  }; 
+  
+  
